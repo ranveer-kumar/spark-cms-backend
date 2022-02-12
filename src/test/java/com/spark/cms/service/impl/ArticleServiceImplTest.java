fd7f8c06662dd97f2f9c76b2e7ae5eb392b81586
ranveer-kumar <ranveer.solr@gmail.com>
2022-02-12 23:10:43 +0530

    Test cases

--- src/test/java/com/spark/cms/service/impl/ArticleServiceImplTest.java
+++ src/test/java/com/spark/cms/service/impl/ArticleServiceImplTest.java
@@ -1,12 +1,13 @@
 package com.spark.cms.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,12 +16,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import com.spark.cms.model.Article;
 import com.spark.cms.repository.ArticleRepository;
-
 
 class ArticleServiceImplTest {
 
@@ -28,73 +28,73 @@ class ArticleServiceImplTest {
 	public void init() {
 		MockitoAnnotations.openMocks(this);
 	}
-	
+
 	@InjectMocks
 	ArticleServiceImpl articleServiceImpl;
 
-//	@Mock
-//	Article article;
-	
 	@Mock
 	ArticleRepository articleRepository;
 
-
-	
 	@Test
 	void testSaveArticle() {
-//		fail("Not yet implemented");
+		// set
+		Article article = new Article();
+		article.setId(1L);
+		article.setArticleType("news");
+		when(articleRepository.save(article)).thenReturn(article);
+
+		// act
+		Article returnObj = articleServiceImpl.saveArticle(article);
+
+		// assert
+		assertNotNull(returnObj);
+		assertEquals("news", returnObj.getArticleType());
 	}
 
 	@Test
-	public void testGetArticleByID() {
-		Optional<Article> articleOptional = Optional.of(Article.builder().id(1L).articleType("news").title("Ttitle").domainId(1).build());
-		when(articleServiceImpl.getArticleByID(1L)).thenReturn(articleOptional);
-		
+	void testGetArticleByID() {
+		Optional<Article> articleOptional = Optional
+				.of(Article.builder().id(1L).articleType("news").title("Ttitle").domainId(1).build());
+		when(articleRepository.findById(1L)).thenReturn(articleOptional);
+
 		Optional<Article> articleObj = articleServiceImpl.getArticleByID(1L);
-		
+
+		assertTrue(articleObj.isPresent());
 		assertEquals("news", articleObj.get().getArticleType());
 		assertEquals("Ttitle", articleObj.get().getTitle());
 		assertEquals(1, articleObj.get().getDomainId());
-		
+
 	}
 
-//	
-//	@Override
-//	public Page<Article> getAllArticles(Pageable pageable) {
-//		log.debug("Before getAllArticles with pageable parameter");
-//		return articleRepository.findAll(pageable);
-//	}
-//	
-	
-//	@Test
-//	void testGetAllArticles() {
-//		
-//		Page<Article> listArticle;
-//		Article article = new Article();
-//		article.setId(1L);
-//		article.setArticleType("news");
-////		listArticle.toList().add(article);
-//		
-////		List<Article> articleList = Article.builder().id(1L).articleType("news").title("Ttitle").domainId(1).build());
-//		when(articleServiceImpl.getAllArticles(PageRequest.of( 0, 10))).thenReturn(listArticle);
-//		
-//		Optional<Article> articleObj = articleServiceImpl.getArticleByID(1L);
-//		
-//		assertEquals("news", articleObj.get().getArticleType());
-//		assertEquals("Ttitle", articleObj.get().getTitle());
-//		assertEquals(1, articleObj.get().getDomainId());
-//		
-//	}
+	@Test
+	void testGetAllArticles() {
+
+		// set
+		Article article = Article.builder().id(1L).articleType("news").build();
+		Page<Article> listArticle = new PageImpl<>(Stream.of(article).collect(Collectors.toList()));
+
+		when(articleRepository.findAll(PageRequest.of(0, 10))).thenReturn(listArticle);
+
+		// act
+		Page<Article> paginatedList = articleServiceImpl.getAllArticles(PageRequest.of(0, 10));
+
+		// assert
+		assertNotNull(paginatedList);
+		assertEquals(1, paginatedList.getTotalElements());
+		assertEquals("news", paginatedList.get().findFirst().get().getArticleType());
+
+	}
 
 	@Test
 	void testDeleteArticle() {
-//		when(articleServiceImpl.deleteArticle(1L)).thenReturn(null);
+		// set
 		Mockito.doNothing().when(articleRepository).deleteById(1L);
-		articleServiceImpl.deleteArticle(1L);
-		Mockito.verify(articleRepository, Mockito.times(1)).deleteById(1L);
-//		assertEquals(null, null);
-	}
-	
 
+		// act
+		articleServiceImpl.deleteArticle(1L);
+
+		// assert
+		Mockito.verify(articleRepository, Mockito.times(1)).deleteById(1L);
+	}
 
 }
