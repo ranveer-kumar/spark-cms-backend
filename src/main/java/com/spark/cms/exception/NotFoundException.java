5e626b77c261bc11bce6c165a68f2056c97dae4f
ranveer-kumar <ranveer.solr@gmail.com>
2022-02-13 23:00:54 +0530

    Test coverage dependencies added Test coverage report increased

--- src/main/java/com/spark/cms/exception/NotFoundException.java
+++ src/main/java/com/spark/cms/exception/NotFoundException.java
@@ -1,8 +1,12 @@
 package com.spark.cms.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends Exception{
 	
 	/**
